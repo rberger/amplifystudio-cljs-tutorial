f287cc4e84899b60b73048ddaa8fd577ef15a8db
Robert J. Berger <rob@omnyway.com>
2022-01-02 19:07:04 -0800

    Changed top element of app in core.cljs

--- src/main/amplifystudio_cljs_tutorial/app/core.cljs
+++ src/main/amplifystudio_cljs_tutorial/app/core.cljs
@@ -1,8 +1,14 @@
 (ns amplifystudio-cljs-tutorial.app.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            ["/aws-exports" :default ^js aws-exports]
+            ["aws-amplify" :refer [Amplify] :as amplify]
+            ["@aws-amplify/ui-react" :refer [AmplifyProvider]]
+            ["ui-components/CardACollection" :default CardACollection]))
 
 (defn app []
-  [:h1 "Create Reagent App"])
+  [:h1 "Amplify Studio Tutorial"]
+  [:> AmplifyProvider
+   [:> CardACollection]])
 
 (defn render []
   (rdom/render [app] (.getElementById js/document "root")))
